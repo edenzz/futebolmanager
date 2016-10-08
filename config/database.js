2896cc69f03c0c9b161985efb51fba1570479dc2
Pedro Fonseca <Pedro Fonseca>
2016-10-08 11:29:57 +0100

    make it better

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ module.exports = {
 
 	// the database url to connect
 	// url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
-	url : 'mongodb://admin:admin@ds035016.mlab.com:35016/futebolmanager'
+	url : 'mongodb://admin:zeca@ds035016.mlab.com:35016/futebolmanager'
 }
